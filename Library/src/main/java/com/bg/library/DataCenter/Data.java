d1ae2e6da9285536eff1e411a69e5b2f38e9e93c
BinGe1987 <shenyaobin@foxmail.com>
2017-11-07 17:10:12 +0800

    DataCenter优化

--- Library/src/main/java/com/bg/library/DataCenter/Data.java
+++ Library/src/main/java/com/bg/library/DataCenter/Data.java
@@ -3,29 +3,30 @@ package com.bg.library.DataCenter;
 import com.bg.library.Base.Objects.JSON.JError;
 import com.bg.library.Base.Objects.JSON.JSON;
 
-import org.json.JSONObject;
-
 /**
  * Created by BinGe on 2017/8/31.
  * 此类为数据中心所有数据的基本类型
  */
 
 public abstract class Data extends JSON {
+
+    /**
+     * 错误信息
+     */
+    public JError error;
+
+    /**
+     * 判断数据是否正常的方法
+     *
+     * @return
+     */
+    public final boolean isDataNormal() {
+        return error == null && !isJSONEmpty();
+    }
+
     /**
      * json数据刷新的时候会调用这个方法
      */
     protected abstract void onData();
-
-    /**
-     * 判断数据是否正常的方法
-     * @return
-     */
-    public abstract boolean isDataNormal();
-
-    /**
-     *
-     * @return
-     */
-    public abstract JError getError();
 
 }
