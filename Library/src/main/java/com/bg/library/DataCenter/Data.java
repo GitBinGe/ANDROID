3872659b0ba16b550729e8a29e1c809a47c8f1a0
BinGe1987 <shenyaobin@foxmail.com>
2017-11-09 20:22:25 +0800

    Data优化

--- Library/src/main/java/com/bg/library/DataCenter/Data.java
+++ Library/src/main/java/com/bg/library/DataCenter/Data.java
@@ -1,17 +1,39 @@
 package com.bg.library.DataCenter;
 
-import com.bg.library.Base.Objects.JSON.JSON;
+import com.bg.library.Base.Objects.JSON.*;
 
 /**
  * Created by BinGe on 2017/8/31.
  * 此类为数据中心所有数据的基本类型
  */
 
-public abstract class Data extends JSON {
+public class Data extends JSON {
+
+
+    private JError error;
+
     /**
      * 判断数据是否正常的方法
      *
      * @return
      */
-    public abstract boolean isDataNormal();
+    public boolean isDataNormal() {
+        return error == null && !isJSONEmpty();
+    }
+
+    /**
+     * 设备错误信息
+     * @param error
+     */
+    public void setError(JError error) {
+        this.error = error;
+    }
+
+    /**
+     * 获取错误信息
+     * @return
+     */
+    public JError getError() {
+        return error;
+    }
 }
